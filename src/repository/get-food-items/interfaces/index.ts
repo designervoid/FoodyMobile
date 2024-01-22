e9ed35b8a7b424da8863aef64f92ce36e74c1e5a
Maxim Chernyshov <dsgnrvd@gmail.com>
2024-01-22 16:04:37 +0200

    yarn lint --fix

--- src/repository/get-food-items/interfaces/index.ts
+++ src/repository/get-food-items/interfaces/index.ts
@@ -1,12 +1,12 @@
 export interface FoodItem {
-  id:            number;
-  name:          null | string;
-  imageUrl:      null | string;
-  foodType:      number;
-  fat:           number;
+  id: number;
+  name: null | string;
+  imageUrl: null | string;
+  foodType: number;
+  fat: number;
   carbohydrates: number;
-  sugar:         number;
-  cholesterol:   number;
+  sugar: number;
+  cholesterol: number;
 }
 
 export type FoodItems = FoodItem[];
--- src/screens/meal/edit/index.tsx
+++ src/screens/meal/edit/index.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
+import {FlatList, Text, View} from 'react-native';
 
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {MealStackParamList} from 'navigators/meal';
-import { useGetMealItem } from 'repository/get-meal-item';
-import { FlatList, Text, View } from 'react-native';
-import { useGetFoodItems } from 'repository/get-food-items';
-import { useGetCalculateRating } from 'repository/get-calculate-rating';
 import BouncyCheckbox from 'react-native-bouncy-checkbox';
+import {useGetCalculateRating} from 'repository/get-calculate-rating';
+import {useGetFoodItems} from 'repository/get-food-items';
+import {useGetMealItem} from 'repository/get-meal-item';
 
 type Props = NativeStackScreenProps<MealStackParamList, 'MealEdit'>;
 
-function Rating({ id }: { id: number | string }) {
-    const swrState = useGetCalculateRating(id.toString());
+function Rating({id}: {id: number | string}) {
+  const swrState = useGetCalculateRating(id.toString());
 
-    if (swrState.isLoading) {
-        return <Text>Loading...</Text>;
-    }
+  if (swrState.isLoading) {
+    return <Text>Loading...</Text>;
+  }
 
-    return <Text>{swrState.data?.rating}</Text>;
+  return <Text>{swrState.data?.rating}</Text>;
 }
 
 export function MealEditScreen(props: Props) {
@@ -25,20 +25,45 @@ export function MealEditScreen(props: Props) {
   const swrState0 = useGetMealItem(id);
   const swrState1 = useGetFoodItems();
 
-  return <>
-    <FlatList data={swrState1.data || []} renderItem={(q) => {
-        return <View style={{ height: 103.29, paddingHorizontal: 20, paddingVertical: 13, borderRadius: 10, backgroundColor: '#fff', marginTop: 17, marginHorizontal: 20, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                <BouncyCheckbox size={25}
-                    fillColor="#3AC2C3"
-                    unfillColor="#FFFFFF"
-                    iconStyle={{ borderColor: "green", borderRadius: 5 }}
-                    innerIconStyle={{ borderWidth: 2, borderRadius: 5, borderColor: '#3AC2C3' }}
-                    isChecked={swrState0.data?.foodItemIds.includes(Number(q.item.id))} 
-                />
-            <Rating id={q.item.id} />
-            <Text style={{ marginLeft: 5 }}>{JSON.stringify(q.item.id)}</Text>
-        </View>;
+  return (
+    <>
+      <FlatList
+        data={swrState1.data || []}
+        renderItem={q => {
+          return (
+            <View
+              style={{
+                height: 103.29,
+                paddingHorizontal: 20,
+                paddingVertical: 13,
+                borderRadius: 10,
+                backgroundColor: '#fff',
+                marginTop: 17,
+                marginHorizontal: 20,
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <BouncyCheckbox
+                size={25}
+                fillColor="#3AC2C3"
+                unfillColor="#FFFFFF"
+                iconStyle={{borderColor: 'green', borderRadius: 5}}
+                innerIconStyle={{
+                  borderWidth: 2,
+                  borderRadius: 5,
+                  borderColor: '#3AC2C3',
+                }}
+                isChecked={swrState0.data?.foodItemIds.includes(
+                  Number(q.item.id),
+                )}
+              />
+              <Rating id={q.item.id} />
+              <Text style={{marginLeft: 5}}>{JSON.stringify(q.item.id)}</Text>
+            </View>
+          );
         }}
-        />
-    </>;
+      />
+    </>
+  );
 }
